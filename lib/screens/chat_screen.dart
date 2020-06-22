41029922a8927391c89d9c8ad3f17ec10862f3aa
inho <inho@inho-macpro.local>
2020-06-22 21:37:57 +0900

    rendering stream data with StreamBuilder

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -5,24 +5,33 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (ctx, index) => Container(
-          padding: EdgeInsets.all(8),
-          child: Text('This works!'),
-        ),
+      body: StreamBuilder(
+        stream: Firestore.instance
+        .collection('chats/6jfDAgZkFhcNqJvapI55/messages')
+        .snapshots(),
+
+        builder: (context, streamSnapshot) {
+          if (streamSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          final documents = streamSnapshot.data.documents;
+          return ListView.builder(
+            itemCount: documents.length,
+            itemBuilder: (ctx, index) => Container(
+              padding: EdgeInsets.all(8),
+              child: Text(documents[index]['text']),
+            ),
+          );
+        }
       ),
+
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
-          Firestore.instance
-          .collection('chats/6jfDAgZkFhcNqJvapI55/messages')
-          .snapshots()
-          .listen((data) {
-              data.documents.forEach((d) {
-                  print(d['text']);
-              });
-          });
+
         },
       ),
     );
