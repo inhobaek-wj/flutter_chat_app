b0b6337cec10997076fcb37f4ca8f9fce9dec481
inho <inho@inho-macpro.local>
2020-06-28 22:33:27 +0900

    added new widget Messages

---
+++ lib/widgets/chat/messages.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Messages extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: Firestore.instance.collection('chat').snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator(),);
+        }
+
+        final chatDocs = snapshot.data.documents;
+        return ListView.builder(
+          itemCount: chatDocs.length,
+          itemBuilder: (ctx,index) => Text(
+            chatDocs[index]['text'],
+          ),
+        );
+      }
+    );
+  }
+}
