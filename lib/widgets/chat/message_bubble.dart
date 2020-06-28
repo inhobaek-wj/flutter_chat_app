899667e9f92c9a47abd1564d205c49d68f5686a2
inho <inho@inho-macpro.local>
2020-06-29 07:37:05 +0900

    show user name using firestore in MessageBubble

--- lib/widgets/chat/message_bubble.dart
+++ lib/widgets/chat/message_bubble.dart
@@ -1,10 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class MessageBubble extends StatelessWidget {
   final String message;
+  final String userId;
   final bool isMe;
   final Key key;
-  const MessageBubble(this.message, this.isMe, {this.key});
+  const MessageBubble(this.message, this.userId, this.isMe, {this.key});
 
   @override
   Widget build(BuildContext context) {
@@ -25,11 +27,37 @@ class MessageBubble extends StatelessWidget {
           padding: EdgeInsets.symmetric(vertical: 10,horizontal: 16,),
           margin: EdgeInsets.symmetric(vertical: 4,horizontal: 8),
 
-          child: Text(
-            message,
-            style: TextStyle(
-              color: isMe? Colors.black : Theme.of(context).accentTextTheme.headline1.color,
-            ),
+          child: Column(
+            crossAxisAlignment: isMe? CrossAxisAlignment.end
+            : CrossAxisAlignment.start,
+            children: [
+
+              FutureBuilder(
+                future: Firestore.instance.collection('users').document(userId).get(),
+                builder: (ctx, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(child: CircularProgressIndicator(),);
+                  }
+
+                  return Text(
+                    snapshot.data['userName'],
+                    style: TextStyle(
+                      color: isMe? Colors.black :
+                      Theme.of(context).accentTextTheme.headline1.color,
+                      fontWeight: FontWeight.bold),
+                  );
+                },
+              ),
+              Text(
+                message,
+                style: TextStyle(
+                  color: isMe? Colors.black
+                  : Theme.of(context).accentTextTheme.headline1.color,
+                ),
+                textAlign: isMe? TextAlign.end
+                : TextAlign.start,
+              ),
+            ],
           ),
         ),
       ],
