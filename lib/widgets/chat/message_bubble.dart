5628b8b62119f327434a68e58bd0c8de1f9aea1a
inho <inho@inho-macpro.local>
2020-06-29 06:45:09 +0900

    wrapping Container with Row in MessageBubble to shrink width

--- lib/widgets/chat/message_bubble.dart
+++ lib/widgets/chat/message_bubble.dart
@@ -7,19 +7,23 @@ class MessageBubble extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Theme.of(context).accentColor,
-        borderRadius: BorderRadius.circular(12),
-      ),
-      width: 140,
-      padding: EdgeInsets.symmetric(vertical: 10,horizontal: 16,),
-      margin: EdgeInsets.symmetric(vertical: 4,horizontal: 8),
+    return Row(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).accentColor,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          width: 140,
+          padding: EdgeInsets.symmetric(vertical: 10,horizontal: 16,),
+          margin: EdgeInsets.symmetric(vertical: 4,horizontal: 8),
 
-      child: Text(
-        message,
-        style: TextStyle(color: Theme.of(context).accentTextTheme.headline1.color),
-      ),
+          child: Text(
+            message,
+            style: TextStyle(color: Theme.of(context).accentTextTheme.headline1.color),
+          ),
+        ),
+      ],
     );
   }
 
