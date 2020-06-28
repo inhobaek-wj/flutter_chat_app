42df20f117408eca0d82002670c71e57e6222f79
inho <inho@inho-macpro.local>
2020-06-28 22:33:50 +0900

    replace body of chat_screen with Messages widget

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../widgets/chat/messages.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
@@ -37,27 +38,14 @@ class ChatScreen extends StatelessWidget {
         ],
       ),
 
-      body: StreamBuilder(
-        stream: Firestore.instance
-        .collection('chats/6jfDAgZkFhcNqJvapI55/messages')
-        .snapshots(),
-
-        builder: (context, streamSnapshot) {
-          if (streamSnapshot.connectionState == ConnectionState.waiting) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-
-          final documents = streamSnapshot.data.documents;
-          return ListView.builder(
-            itemCount: documents.length,
-            itemBuilder: (ctx, index) => Container(
-              padding: EdgeInsets.all(8),
-              child: Text(documents[index]['text']),
+      body: Container(
+        child: Column(
+          children: [
+            Expanded(
+              child: Messages(),
             ),
-          );
-        }
+          ],
+        ),
       ),
 
       floatingActionButton: FloatingActionButton(
