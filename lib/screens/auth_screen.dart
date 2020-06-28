cad7efa05a1162a6e2226bdaacd2841273e7334a
inho <inho@inho-macpro.local>
2020-06-28 12:21:31 +0900

    implemented auth thing using firebase with little error handling

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
 
 import '../widgets/auth_form.dart';
 
@@ -8,9 +10,39 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  final _auth = FirebaseAuth.instance;
 
-  void _submitAuthForm(String email,String password,String userName,bool isLogin) {
-    // create user or sign an existing user using firebase.
+  void _submitAuthForm(
+    String email,String password,String userName,bool isLogin, BuildContext ctx) async {
+    AuthResult _authResult;
+
+    try {
+      if (isLogin) {
+        _authResult = await _auth.signInWithEmailAndPassword(
+          email: email,password: password);
+      } else {
+        // create user or sign an existing user using firebase.
+        _authResult = await _auth.createUserWithEmailAndPassword(
+          email: email,password: password);
+      }
+
+    } on PlatformException catch (err) {
+      var message = 'An error occurred, please check your credentials';
+
+      if (err.message != null ) {
+        message = err.message;
+      }
+
+      Scaffold.of(ctx).showSnackBar(
+        SnackBar(
+          content: Text(message),
+          backgroundColor: Theme.of(ctx).errorColor,
+      ));
+    } catch (err) {
+      print(err);
+    }
+
+
   }
 
   @override
