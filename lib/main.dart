c6bd6605befbb09b50dea4c1c8e6687073567682
inho <inho@inho-macpro.local>
2020-06-28 22:05:03 +0900

    using StreamBuilder to listen FirebaseAuth.instance.onAuthStateChanged

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 import 'screens/auth_screen.dart';
+import 'screens/chat_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -25,7 +27,15 @@ class MyApp extends StatelessWidget {
         ),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: AuthScreen(),
+      home: StreamBuilder<Object>(
+        stream: FirebaseAuth.instance.onAuthStateChanged,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ChatScreen();
+          }
+          return AuthScreen();
+        }
+      ),
     );
   }
 }
