f86998982b7514ec53aa2d168d8054537733f189
inho <inho@inho-macpro.local>
2020-06-29 16:11:05 +0900

    trying firebase messaging push notification event handlers

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,10 +1,36 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 import '../widgets/chat/messages.dart';
 import '../widgets/chat/new_message.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  void initState() {
+    final fbm = FirebaseMessaging();
+    // this code below is for iOS, I actually did not set up for iOS push notification.
+    fbm.requestNotificationPermissions();
+    fbm.configure(
+      onMessage: (msg) {
+        return;
+      },
+      onLaunch: (msg) {
+        return;
+      },
+      onResume: (msg) {
+        return;
+      },
+    );
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
