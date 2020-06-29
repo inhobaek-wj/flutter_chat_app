92b936c3bd9efde3e1fc5cae08b7f83a01e2fc6a
inho <inho@inho-macpro.local>
2020-06-29 16:35:40 +0900

    testing firebase function, need to deploy it before test

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,11 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.myFunction = functions.firestore
+  .document('chat/{message}')
+  .onCreate((snapshot,context) => {
+    console.log(snapshot.data());
+
+    return;
+  });
